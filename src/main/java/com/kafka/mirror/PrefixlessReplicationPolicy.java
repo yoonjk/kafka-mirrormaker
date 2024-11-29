7890da5c2cbb2a67ba2ad11481835f7b27e5a8d8
yoonjk <nexweb1@gmail.com>
2024-11-29 18:51:47 +0900

    Kafka Mirrormaker

--- src/main/java/com/kafka/mirror/PrefixlessReplicationPolicy.java
+++ src/main/java/com/kafka/mirror/PrefixlessReplicationPolicy.java
@@ -7,26 +7,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PrefixlessReplicationPolicy extends DefaultReplicationPolicy {
-	  private static final Logger log = LoggerFactory.getLogger(PrefixlessReplicationPolicy.class);
+	private static final Logger log = LoggerFactory.getLogger(PrefixlessReplicationPolicy.class);
 
-	  @Override
-	  public void configure(Map<String, ?> props) {
-	    super.configure(props);	    
-	  }
+	@Override
+	public void configure(Map<String, ?> props) {
+		super.configure(props);
+		log.info("props:{}", props);
+	}
 
-	  @Override
-	  public String formatRemoteTopic(String sourceClusterAlias, String topic) {
-	    return topic;
-	  }
+	@Override
+	public String formatRemoteTopic(String sourceClusterAlias, String topic) {
 
-	  @Override
-	  public String topicSource(String topic) {
-	    // return topic == null ? null : sourceClusterAlias;
-	    return null;
-	  }
+		log.info("sourceClusterAlias:{}. topic:{}", sourceClusterAlias, topic);
+		
+		return topic;
+	}
 
-	  @Override
-	  public String upstreamTopic(String topic) {
-	    return null;
-	  }
+	@Override
+	public String topicSource(String topic) {
+
+		return null;
+	}
+
+	@Override
+	public String upstreamTopic(String topic) {
+		
+		return null;
+	}
 }
